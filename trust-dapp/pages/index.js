e7b72ca040da7cfa1e9c7743fb4dd0f4102d9bbe
Kuraj11101 <58647906+Kuraj11101@users.noreply.github.com>
2022-05-13 18:59:55 +0000

    Update

--- trust-dapp/pages/index.js
+++ trust-dapp/pages/index.js
@@ -14,6 +14,9 @@ import Script from 'next/script';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+
+<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
+<Script src="https://unpkg.com/react/umd/react.production.min.js" crossOrigin></Script>
   // ETH Balance of the DAO contract
   const [treasuryBalance, setTreasuryBalance] = useState("0");
   // Number of proposals created in the DAO
@@ -350,10 +353,6 @@ export default function Home() {
         <meta name="description" content="Trust dApp" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
-        <Script src="https://unpkg.com/react/umd/react.production.min.js" crossOrigin></Script>
-
-
       </Head>
 
       <div className={styles.main}>
